036130720a3738351234dc97e864454d2213fe2a
ortizk <ortizk@live.com>
2024-07-30 13:57:03 -0700

    added input box and add to-tdo button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 export default function Page() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+      <div>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          <li style={{ padding: "5px 0" }}>To Do 1</li>
+          <li style={{ padding: "5px 0" }}>To Do 2</li>
+          <li style={{ padding: "5px 0" }}>To Do 3</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
