7eef0a2b86c00019b32e2b045236a2f8d14ae455
ortizk <ortizk@live.com>
2024-07-30 14:06:28 -0700

    moved addnewtodo and todolist to own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
+import AddNewTodo from "./add-new-todo";
+import ToDoList from "./todo-list";
+
 export default function Page() {
   return (
     <div>
-      <div>
-        <input type="text" />
-        <button>Add</button>
-      </div>
-      <div>
-        <ul style={{ listStyleType: "none", padding: 0 }}>
-          <li style={{ padding: "5px 0" }}>To Do 1</li>
-          <li style={{ padding: "5px 0" }}>To Do 2</li>
-          <li style={{ padding: "5px 0" }}>To Do 3</li>
-        </ul>
-      </div>
+      <AddNewTodo />
+      <ToDoList />
     </div>
   );
 }
---
+++ app/todo-list.tsx
@@ -0,0 +1,11 @@
+export default function ToDoList() {
+  return (
+    <div>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        <li style={{ padding: "5px 0" }}>To Do 1</li>
+        <li style={{ padding: "5px 0" }}>To Do 2</li>
+        <li style={{ padding: "5px 0" }}>To Do 3</li>
+      </ul>
+    </div>
+  );
+}
