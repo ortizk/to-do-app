157b658179f6b4f787ebdda97925b65f19336e08
ortizk <ortizk@live.com>
2024-08-05 15:15:04 -0700

    added more isDone functionality

--- app/todo.tsx
+++ app/todo.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-async function update(id: string, isDone: boolean) {
+import { useRouter } from "next/navigation";
+
+async function update(id: string, isDone: boolean, refresh) {
   await fetch("http://localhost:3001/api/todo/update", {
     method: "POST",
     mode: "no-cors",
     body: JSON.stringify({ id, isDone }),
   });
+  refresh();
 }
 
 export default function Todo({ todo }) {
+  const router = useRouter();
   return (
     <>
       <input
         type="checkbox"
-        onChange={(e) => update(todo.id, e.target.checked)}
+        onChange={(e) => update(todo.id, e.target.checked, router.refresh)}
+        checked={todo.isDone}
       />
       <span>{todo.name}</span>
       <button>Delete</button>
