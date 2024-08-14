5563a6d456f3515e867467e135d94b77734b4a8d
ortizk <ortizk@live.com>
2024-08-14 15:48:29 -0700

    small changes before branching

--- app/todo.tsx
+++ app/todo.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from "next/navigation";
 
 async function update(id: string, isDone: boolean, refresh) {
-  await fetch(`http://localhost:3001/api/todo/update`, {
+  await fetch(`/api/todo/update`, {
     method: "POST",
     mode: "no-cors",
     body: JSON.stringify({ id, isDone }),
